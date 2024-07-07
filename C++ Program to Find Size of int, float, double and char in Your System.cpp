d3406f2d66d7d4168f90e06bc396f5cadac01206
Aditya Kamble <164652674+AdityaKamble06@users.noreply.github.com>
2024-07-07 11:22:31 +0530

    Create C++ Program to Find Size of int, float, double and char in Your System.cpp

---
+++ C++ Program to Find Size of int, float, double and char in Your System.cpp
@@ -0,0 +1,20 @@
+//Example: Find Size of a Variable
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ cout << "Size of char: " << sizeof(char) <<" byte" << endl;
+ cout << "Size of int: " << sizeof(int) <<" bytes" << endl;
+ cout << "Size of float: " << sizeof(float) <<" bytes" << endl;
+ cout << "Size of double: " << sizeof(double) <<" bytes" << endl;
+ 
+ return 0;
+}
+
+//Output
+Size of char: 1 byte
+Size of int: 4 bytes
+Size of float: 4 bytes
+Size of double: 8 bytes

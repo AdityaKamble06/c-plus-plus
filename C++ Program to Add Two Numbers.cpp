dfa9123af8fac8d0dacb2f08524190e7a0e5fb6d
Aditya Kamble <164652674+AdityaKamble06@users.noreply.github.com>
2024-07-07 11:13:09 +0530

    Create C++ Program to Add Two Numbers.cpp

---
+++ C++ Program to Add Two Numbers.cpp
@@ -0,0 +1,24 @@
+//Example: Program to Add Two Integers
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int first_number, second_number, sum;
+ 
+ cout << "Enter two integers: ";
+ cin >> first_number >> second_number;
+ 
+ //sum of two numbers is stored in variable sumOfTwoNumbers
+ sum = first_number + second_number;
+ 
+ //prints sum
+ cout << first_number <<" + "<< second_number << " = " <<sum;
+ 
+ return 0;
+}
+
+//Output
+Enter two integers: 23 34
+23 + 34 = 57

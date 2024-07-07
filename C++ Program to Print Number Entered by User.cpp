d90d7c528de31a97ed740396df92aa4d3d07f0e7
Aditya Kamble <164652674+AdityaKamble06@users.noreply.github.com>
2024-07-07 11:08:12 +0530

    Create C++ Program to Print Number Entered by User.cpp

---
+++ C++ Program to Print Number Entered by User.cpp
@@ -0,0 +1,19 @@
+//Example: Print Number Entered by User
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int number;
+ 
+ cout << "Enter an integer: ";
+ cin >> number;
+ 
+ cout << "You entered: " << number;
+ return 0;
+}
+
+//Output
+Enter an integer: 34
+You entered: 34

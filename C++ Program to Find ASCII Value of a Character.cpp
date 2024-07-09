735fa7457475856aad010b0201be2b90f9d78aaa
Aditya Kamble <164652674+AdityaKamble06@users.noreply.github.com>
2024-07-09 21:49:37 +0530

    Create C++ Program to Find ASCII Value of a Character.cpp

---
+++ C++ Program to Find ASCII Value of a Character.cpp
@@ -0,0 +1,17 @@
+//Example: Print ASCII Value in C++
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ char c;
+ cout << "Enter a character: ";
+ cin >> c;
+ cout << "ASCII value of " << c << " is " << int(c);
+ return 0;
+}
+
+//Output
+Enter a character: p
+ASCII value of p is 112
